7adf81a16f7eb70833e4b719c5ae2a3c2695263e
Nuwantha Dewappriya <nuwanthadew@gmail.com>
2016-03-05 12:15:35 +0530

    tank get life pack when health less than 60

--- src/AI/AIController.java
+++ src/AI/AIController.java
@@ -45,32 +45,54 @@ public class AIController {
             //check whether other tank are in same x axis or y axis with me amd no bariyers between us
             int distroyTank = Mode.distroyTank(mytank,memory,map);
 
+
             if(distroyTank==0) {
-                //if no tank in x axis or y axis
-                //get suitable coin for target
-                for (Coin_Pile coin_pile : map.getCoinPiles()) {
-                       int xCoordinate = coin_pile.getX() - x;
-                       int yCoordinate = coin_pile.getY() - y;
-                       if (Math.abs(xCoordinate) + Math.abs(yCoordinate) < Math.abs(newX - x) + Math.abs(newY - y)) {
-                           newX = coin_pile.getX();
-                           newY = coin_pile.getY();
-                           move = true;
-                       }
-                }
-
-                // if no coin pile in current of health less than 100 try to get nearlest health pack
-                if (!move || mytank.getHealth() < 100) {
-                       for (Life_Pack life_pack : map.getLifePacks()) {
-                           int xCoordinate = life_pack.getX() - x;
-                           int yCoordinate = life_pack.getY() - y;
-                           if (Math.abs(xCoordinate) + Math.abs(yCoordinate) < Math.abs(newX - x) + Math.abs(newY - y)) {
-                               newX = life_pack.getX();
-                               newY = life_pack.getY();
-                               move = true;
-                           }
-
-                       }
-
+                //if health less than 60 go to find life pack only if no life pack go to find coins.
+                if(mytank.getHealth()<60) {
+                    for (Life_Pack life_pack : map.getLifePacks()) {
+                        int xCoordinate = life_pack.getX() - x;
+                        int yCoordinate = life_pack.getY() - y;
+                        if (Math.abs(xCoordinate) + Math.abs(yCoordinate) < Math.abs(newX - x) + Math.abs(newY - y)) {
+                            newX = life_pack.getX();
+                            newY = life_pack.getY();
+                            move = true;
+                        }
+                    }
+                    if (!move) {
+                        for (Coin_Pile coin_pile : map.getCoinPiles()) {
+                            int xCoordinate = coin_pile.getX() - x;
+                            int yCoordinate = coin_pile.getY() - y;
+                            if (Math.abs(xCoordinate) + Math.abs(yCoordinate) < Math.abs(newX - x) + Math.abs(newY - y)) {
+                                newX = coin_pile.getX();
+                                newY = coin_pile.getY();
+                                move = true;
+                            }
+                        }
+                    }
+                }else {
+                    //if no tank in x axis or y axis
+                    //get suitable coin for target
+                    for (Coin_Pile coin_pile : map.getCoinPiles()) {
+                        int xCoordinate = coin_pile.getX() - x;
+                        int yCoordinate = coin_pile.getY() - y;
+                        if (Math.abs(xCoordinate) + Math.abs(yCoordinate) < Math.abs(newX - x) + Math.abs(newY - y)) {
+                            newX = coin_pile.getX();
+                            newY = coin_pile.getY();
+                            move = true;
+                        }
+                    }
+                    // if no coin pile in current of health less than 150 try to get nearlest health pack
+                    if (!move || mytank.getHealth() < 150) {
+                        for (Life_Pack life_pack : map.getLifePacks()) {
+                            int xCoordinate = life_pack.getX() - x;
+                            int yCoordinate = life_pack.getY() - y;
+                            if (Math.abs(xCoordinate) + Math.abs(yCoordinate) < Math.abs(newX - x) + Math.abs(newY - y)) {
+                                newX = life_pack.getX();
+                                newY = life_pack.getY();
+                                move = true;
+                            }
+                        }
+                    }
                 }
                 MoveChooser moveChooser=new MoveChooser(AIController.map,AIController.mytank);
                 return moveChooser.shortestPathCalculator(x, y, newX, newY, 1);
